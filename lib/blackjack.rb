df66ee906e409b24f07b7a758d7b042a7d3551f1
Lauren Yu <laurenkyu@gmail.com>
2020-01-25 04:49:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,9 +59,8 @@ def runner
   welcome 
   card_total = initial_round
   until card_total > 21
-  hit?
-  display_card_total until > 21
-  end_game
-  
+  card_total = hit?(card_total)
+  display_card_total(card_total)
+  end
+  end_game(card_total)
 end
-    
